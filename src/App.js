5dab75c0825d24b327e1a98e3282f8a417ba755d
carlmenke <carlmenke13@gmail.com>
2022-08-30 08:40:50 -0700

    added a second text div and used a second tardis state const to make them upate indivuall

--- src/App.js
+++ src/App.js
@@ -1,43 +1,86 @@
 import './App.css';
-import React , {useState, useEffect} from 'react'
+import React , {useState} from 'react'
 
 function App() {
 
-  const [tardis, manageTardis] = useState({
+  const DivOne = (props) => {
+    return (
+        <div>       
+          <DivTwo handleClick = {props.handleClick} /> 
+        </div>
+    )
+  }
+    const DivTwo = (props) => {
+      return (
+          <div>       
+            <DivThree tardis = {tardisOne} handleClick = {props.handleClick} /> 
+            <DivThree tardis = {tardisTwo} handleClick = {props.handleClick} /> 
+          </div>
+      )
+    }
+    const DivThree = (props) => {
+      return (
+          <div> {console.log(props.tardis.num)}      
+            <h3 onClick = {() => {props.handleClick(props.tardis)}}>{props.tardis.taco}</h3>
+          </div>
+      )
+    }
+
+  const [tardisOne, manageTardisOne] = useState({
     taco: 'Time and Relative Dimension in Space',
     caps: false,
+    num: 1
+  })
+  const [tardisTwo, manageTardisTwo] = useState({
+    taco: 'Time and Relative Dimension in Space',
+    caps: false,
+    num: 2
   })
 
-  useEffect(() => {
-    //Runs only on the first render
-  }, [tardis]);
+  {console.log('tardis one: ', tardisOne)}
+  {console.log('tardis two: ', tardisTwo)}
 
-  const handleClick = () => {
-    if(tardis.caps){
-      console.log('inside handle click funcbiton caps true')
-      console.log(tardis)
-      let newTardis = {}
-      newTardis.taco = tardis.taco.toUpperCase()
-      newTardis.caps = false;
-      manageTardis(newTardis)
-      console.log(tardis)
-    }else {
-      console.log('inside handle click funcbiton caps false')
-      console.log(tardis)
-      let newTardis = {};
-      newTardis.taco = tardis.taco.toLowerCase()
-      newTardis.caps = true;
-      manageTardis(newTardis)
-      console.log(tardis)
+
+  const handleClick = (tardis) => {
+
+      if(tardis.num === 1){
+      if(tardis.caps){
+        let newTardis = {}
+        newTardis.taco = tardis.taco.toUpperCase()
+        newTardis.caps = false;
+        newTardis.num = tardis.num;
+        manageTardisOne(newTardis)
+      }else {
+        let newTardis = {};
+        newTardis.taco = tardis.taco.toLowerCase()
+        newTardis.caps = true;
+        newTardis.num = tardis.num;
+        manageTardisOne(newTardis)
+      }
+    }else{
+      if(tardis.caps){
+        let newTardis = {}
+        newTardis.taco = tardis.taco.toUpperCase()
+        newTardis.caps = false;
+        newTardis.num = tardis.num;
+        manageTardisTwo(newTardis)
+      }else {
+        let newTardis = {};
+        newTardis.taco = tardis.taco.toLowerCase()
+        newTardis.caps = true;
+        newTardis.num = tardis.num;
+        manageTardisTwo(newTardis)
+      }
     }
-  }
+}
 
 
-  return (
+  return(
     <div className="App">
-      <h3 onClick = {handleClick}>{tardis.taco}</h3>
+      <DivOne handleClick = {handleClick} /> 
     </div>
   );
 }
+
 
 export default App;
