1383570c84fb79b0f0eedbdc6e926fb9913d440b
carlmenke <carlmenke13@gmail.com>
2022-08-29 22:46:23 -0700

    added upper to lower case function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState, useEffect} from 'react'
 
 function App() {
+
+  const [tardis, manageTardis] = useState({
+    taco: 'Time and Relative Dimension in Space',
+    caps: false,
+  })
+
+  useEffect(() => {
+    //Runs only on the first render
+  }, [tardis]);
+
+  const handleClick = () => {
+    if(tardis.caps){
+      console.log('inside handle click funcbiton caps true')
+      console.log(tardis)
+      let newTardis = {}
+      newTardis.taco = tardis.taco.toUpperCase()
+      newTardis.caps = false;
+      manageTardis(newTardis)
+      console.log(tardis)
+    }else {
+      console.log('inside handle click funcbiton caps false')
+      console.log(tardis)
+      let newTardis = {};
+      newTardis.taco = tardis.taco.toLowerCase()
+      newTardis.caps = true;
+      manageTardis(newTardis)
+      console.log(tardis)
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 onClick = {handleClick}>{tardis.taco}</h3>
     </div>
   );
 }
